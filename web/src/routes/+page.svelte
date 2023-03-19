523d0f82a2cda1b0127c6cf1d09aab57a07526ed
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-03-19 19:43:09 +0100

    Added a basic sveltekit + tailwind project in the project

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1 class="text-3xl font-bold text-center">
+    Hello world!
+  </h1>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
+  
