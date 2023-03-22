855c7ab0ab078f7738b2a27c6fe14c54cbe8c4a0
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-03-22 11:48:28 +0100

    Properly catch requests to the server and redirect them to API container

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -1,8 +1,11 @@
 import type { HandleFetch } from "@sveltejs/kit";
 
 export const handleFetch = (({ request, fetch }) => {
+  const regex = new RegExp(
+    "http://(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]).)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9-]*[A-Za-z0-9])/api/"
+  );
   request = new Request(
-    request.url.replace("http://localhost/api/", "http://api:9124/"),
+    request.url.replace(regex, "http://api:9124/"),
     request
   );
 
