e6b2fc0941a08c991279c2ae94388df91368ca05
Steel <mael.acier@ensiie.fr>
2023-03-24 19:36:25 +0100

    refactors: move nginx proxy to vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,4 +3,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://api:9124",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
