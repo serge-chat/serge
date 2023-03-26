242292fdad181ad97be52862ad40070a3909d1c5
Henrique Massareli <henrique@oneseventech.com>
2023-03-25 22:08:14 -0300

    feat: handle manual downloads of models

--- api/utils/download.py
+++ api/utils/download.py
@@ -29,16 +29,25 @@ def parse_args():
 
 def download_models(models: List[str]):
     for model in models:
+        if os.path.isfile(f"weights/ggml-alpaca-{model}-q4_0.bin"):
+            print(f"The model ggml-alpaca-{model}-q4_0.bin already exists in the weights/ folder, skipping download.")
+            continue
+        print("Downloading models from HuggingFace")
         repo_id, filename = models_info[model]
         print(f"Downloading {model} model from {repo_id}...")
 
-        huggingface_hub.hf_hub_download(
-            repo_id=repo_id,
-            filename=filename,
-            local_dir="/usr/src/app/weights",
-            local_dir_use_symlinks=False,
-            cache_dir="/usr/src/app/weights/.cache",
-        )
+        try :
+            huggingface_hub.hf_hub_download(
+                repo_id=repo_id,
+                filename=filename,
+                local_dir="/usr/src/app/weights",
+                local_dir_use_symlinks=False,
+                cache_dir="/usr/src/app/weights/.cache",
+            )
+        except :
+            if(model != "tokenizer"):
+                print(f"Download failed, try it again later. You can also download the model manually from https://huggingface.co/Pi3141/alpaca-{model}-ggml/resolve/main/ggml-model-q4_0.bin and put it in the weights/ folder.")
+                print(f"Then rename it to ggml-alpaca-{model}-q4_0.bin and run the script again.")
 
         if filename == "ggml-model-q4_0.bin":
             os.rename(
@@ -49,7 +58,6 @@ def download_models(models: List[str]):
 if __name__ == "__main__":
     args = parse_args()
 
-    print("Downloading models from HuggingFace")
     download_models(args.model)
 
     print("Converting models to the current format")
