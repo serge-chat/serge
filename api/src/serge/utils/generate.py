a28bd548fe7d97fd9c0d814801cc6d1ee50f1179
John Cadengo <john@outreachgrid.com>
2023-04-04 15:05:10 -0700

    Use as much context as possible without exceeding max context window size
    - Go in reverse order to get previous instructions and responses
    - Continue building the full prompt until we exceed the context window size
    - Use the version of the prompt that does not exceed the context window size

--- api/src/serge/utils/generate.py
+++ api/src/serge/utils/generate.py
@@ -2,6 +2,7 @@ import subprocess, os
 from serge.models.chat import Chat, ChatParameters
 import asyncio
 import logging
+import math
 
 logger = logging.getLogger(__name__)
 
@@ -71,14 +72,30 @@ async def get_full_prompt_from_chat(chat: Chat, simple_prompt: str):
     await chat.parameters.fetch_link(ChatParameters.init_prompt)
 
     prompt = chat.parameters.init_prompt + "\n\n"
-    
+    prompt_list = [prompt]
+
+    # Go in reverse order and add in prompts until we reach the max context window size
     if chat.questions != None:
-        for question in chat.questions:
+        for question in chat.questions[::-1]: 
             if question.error != None: # skip errored out prompts
                 continue
-            prompt += "### Instruction:\n" + question.question + "\n"
-            prompt += "### Response:\n" + question.answer + "\n"
+            tmp_prompt_list = prompt_list.copy()
+            tmp_prompt_list.insert(1, "### Instruction:\n" + question.question + "\n")
+            tmp_prompt_list.insert(2, "### Response:\n" + question.answer + "\n")
 
+            # The full prompt includes these at the end of the list
+            tmp_full_prompt_list = tmp_prompt_list.copy()
+            tmp_full_prompt_list += ["### Instruction:\n" + simple_prompt + "\n"]
+            tmp_full_prompt_list += ["### Response:\n"]
+
+            tmp_full_prompt = str.join('', tmp_full_prompt_list)
+            num_tokens = math.ceil(len(tmp_full_prompt) / 4) 
+            if num_tokens < (chat.parameters.context_window - 4):
+                prompt_list = tmp_prompt_list.copy()
+            else:
+                break
+
+    prompt = str.join('', prompt_list)
     prompt += "### Instruction:\n" + simple_prompt + "\n"
     prompt += "### Response:\n"
 
