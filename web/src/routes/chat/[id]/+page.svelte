00ec84b7a0d5fe05ffb0f086457250ac1c19f9c0
pabl-o-ce <cye@poscye.com>
2023-04-23 23:27:39 -0500

    Style: fix chat padding md:responsive

--- web/src/routes/chat/[id]/+page.svelte
+++ web/src/routes/chat/[id]/+page.svelte
@@ -10,6 +10,7 @@
   $: history = data.chat.history;
 
   $: prompt = "";
+  let container;
 
   async function askQuestion() {
     if (prompt) {
@@ -99,7 +100,7 @@
   on:keydown={handleKeyDown}
 >
   <div class="w-full border-b border-[#373d49]">
-    <div class="flex justify-between items-center px-2 md:px-10">
+    <div class="flex justify-between items-center px-2 md:px-16">
       <a class="" href="https://github.com/nsarrazin/serge" target="_blank" rel="noopener noreferrer">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="24" height="24">
           <path class="fill-neutral-content" d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
@@ -133,7 +134,7 @@
     <div class="h-max pb-32">
       {#each history as question}
         {#if question.type === "human"}
-          <div class="chat chat-start px-10 py-4 bg-base-200 border-t border-[#373d49]">
+          <div class="chat chat-start px-10 md:px-16 py-4 bg-base-200 border-t border-[#373d49]">
             <div class="chat-image self-start avatar pl-1 pt-1">
               <div class="w-[2.6rem] bg-gradient-to-b from-blue-500 to-indigo-900 mask mask-squircle online">
                 <svg class="my-2 mx-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="24" height="24">
@@ -151,7 +152,7 @@
             </div>
           </div>
         {:else if question.type === "ai"}
-          <div class="chat chat-start px-10 py-4 bg-base-100 border-t border-[#373d49]">
+          <div class="chat chat-start px-10 md:px-16 py-4 bg-base-100 border-t border-[#373d49]">
             <div class="chat-image self-start avatar pl-1 pt-1">
               <div class="w-[2.6rem] bg-gradient-to-t from-primary to-purple-400 mask mask-squircle online">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="48" height="48">
