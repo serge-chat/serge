4970865a49a5ca8ef0e82cb5b3223f38dbec5c0a
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2023-06-11 20:39:11 -0400

    Add support for validating shell scripts (#416)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Install python bindings
 pip install llama-cpp-python==0.1.62
 
+# Start Redis instance
 redis-server /etc/redis/redis.conf &
+
 # Start the API
 cd api && uvicorn src.serge.main:app --host 0.0.0.0 --port 8008 &
 
 # Wait for any process to exit
 wait -n
-  
+
 # Exit with status of process that exited first
 exit $?
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Install python dependencies
 pip install -e ./api
-pip install llama-cpp-python
 
+# Install python bindings
+pip install llama-cpp-python==0.1.62
+
+# Start Redis instance
 redis-server /etc/redis/redis.conf &
 
 # Start the web server
 cd web && npm run dev -- --host 0.0.0.0 --port 8008 &
-  
+
 # Start the API
 cd api && uvicorn src.serge.main:api_app --reload --host 0.0.0.0 --port 9124 --root-path /api/ &
 
