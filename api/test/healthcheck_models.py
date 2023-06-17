51b30e78a938abb4128903d0b9591abeb13bd543
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-06-17 17:07:10 +0200

    Update models list from a remote location (#436)
    Co-authored-by: Juan Calderon-Perez <835733+gaby@users.noreply.github.com>

---
+++ api/test/healthcheck_models.py
@@ -0,0 +1,27 @@
+import json
+import requests
+import huggingface_hub
+import pytest
+
+from pathlib import Path
+
+# this test file specifically doesn't start with test_* so it's not picked up by pytest
+
+test_dir = Path(__file__).parent
+with open(test_dir.parent / "src/serge/data/models.json", "r") as models_file:
+    families = json.load(models_file)
+
+# generate list of checks
+checks = []
+for family in families:
+    for model in family["models"]:
+        for file in model["files"]:
+            checks.append((model["repo"], file["filename"]))
+
+
+@pytest.mark.parametrize("repo,filename", checks)
+def test_model_available(repo, filename):
+    url = huggingface_hub.hf_hub_url(repo, filename, repo_type="model", revision="main")
+    r = requests.head(url)
+
+    assert r.ok, f"Model {repo}/{filename} not available"
