13e65da70ae3c5fa9c27e76eee0ec5d62f947e5c
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-06-17 12:10:41 +0200

    fix ci

--- api/test/healthcheck_models.py
+++ api/test/healthcheck_models.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 test_dir = Path(__file__).parent
 
-with open(test_dir.parent/'models.json', 'r') as models_file:
+with open(test_dir.parent / "models.json", "r") as models_file:
     families = json.load(models_file)
 
 # generate list of checks
@@ -15,15 +15,12 @@ checks = []
 for family in families:
     for model in family["models"]:
         for file in model["files"]:
-            checks.append((model["repo"],
-                        file["filename"]))
+            checks.append((model["repo"], file["filename"]))
 
 
-@pytest.mark.parametrize(
-    "repo,filename", checks
-)
+@pytest.mark.parametrize("repo,filename", checks)
 def test_model_available(repo, filename):
-        url = huggingface_hub.hf_hub_url(repo, filename, repo_type="model", revision="main")
-        r = requests.head(url)
-        
-        assert r.ok, f"Model {repo}/{filename} not available"
+    url = huggingface_hub.hf_hub_url(repo, filename, repo_type="model", revision="main")
+    r = requests.head(url)
+
+    assert r.ok, f"Model {repo}/{filename} not available"
