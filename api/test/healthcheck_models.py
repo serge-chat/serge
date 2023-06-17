6ba7ecc997ca27192923f799916cb00fc1375af2
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-06-17 12:08:35 +0200

    begin work on model refactoring

---
+++ api/test/healthcheck_models.py
@@ -0,0 +1,29 @@
+import json
+import requests
+import huggingface_hub
+import pytest
+
+from pathlib import Path
+
+test_dir = Path(__file__).parent
+
+with open(test_dir.parent/'models.json', 'r') as models_file:
+    families = json.load(models_file)
+
+# generate list of checks
+checks = []
+for family in families:
+    for model in family["models"]:
+        for file in model["files"]:
+            checks.append((model["repo"],
+                        file["filename"]))
+
+
+@pytest.mark.parametrize(
+    "repo,filename", checks
+)
+def test_model_available(repo, filename):
+        url = huggingface_hub.hf_hub_url(repo, filename, repo_type="model", revision="main")
+        r = requests.head(url)
+        
+        assert r.ok, f"Model {repo}/{filename} not available"
