1d7b7a634a3ae587710fab8c31d92475f001239e
Juan Calderon-Perez <jgcalderonperez@protonmail.com>
2023-07-09 18:14:56 -0400

    Format the .sh files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,17 @@
 set -x
 
 # Install python bindings
-pip install llama-cpp-python==0.1.69 || { echo 'Failed to install llama-cpp-python' ; exit 1; }
+pip install llama-cpp-python==0.1.69 || {
+	echo 'Failed to install llama-cpp-python'
+	exit 1
+}
 
 # Start Redis instance
 redis-server /etc/redis/redis.conf &
 
 # Start the API
 cd /usr/src/app/api
-uvicorn src.serge.main:app --host 0.0.0.0 --port 8008 || { echo 'Failed to start main app' ; exit 1; }
+uvicorn src.serge.main:app --host 0.0.0.0 --port 8008 || {
+	echo 'Failed to start main app'
+	exit 1
+}
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -3,10 +3,16 @@
 set -x
 
 # Install python dependencies
-pip install -e ./api  || { echo 'Failed to install python dependencies' ; exit 1; }
+pip install -e ./api || {
+	echo 'Failed to install python dependencies'
+	exit 1
+}
 
 # Install python bindings
-pip install llama-cpp-python==0.1.69 || { echo 'Failed to install llama-cpp-python' ; exit 1; }
+pip install llama-cpp-python==0.1.69 || {
+	echo 'Failed to install llama-cpp-python'
+	exit 1
+}
 
 # Start Redis instance
 redis-server /etc/redis/redis.conf &
@@ -17,4 +23,7 @@ npm run dev -- --host 0.0.0.0 --port 8008 &
 
 # Start the API
 cd /usr/src/app/api
-uvicorn src.serge.main:api_app --reload --host 0.0.0.0 --port 9124 --root-path /api/ || { echo 'Failed to start main app' ; exit 1; }
+uvicorn src.serge.main:api_app --reload --host 0.0.0.0 --port 9124 --root-path /api/ || {
+	echo 'Failed to start main app'
+	exit 1
+}
