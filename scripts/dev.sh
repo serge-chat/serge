696c2d288c9510441af89a8b1ac4bfc2ed403531
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2023-07-09 18:28:33 -0400

    Fixes to startup scripts and Dockerfiles (#517)

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+set -x
+
 # Install python dependencies
-pip install -e ./api
+pip install -e ./api || {
+	echo 'Failed to install python dependencies'
+	exit 1
+}
 
 # Install python bindings
-pip install llama-cpp-python==0.1.62
+pip install llama-cpp-python==0.1.69 || {
+	echo 'Failed to install llama-cpp-python'
+	exit 1
+}
 
 # Start Redis instance
 redis-server /etc/redis/redis.conf &
 
 # Start the web server
-cd web && npm run dev -- --host 0.0.0.0 --port 8008 &
+cd /usr/src/app/web || exit 1
+npm run dev -- --host 0.0.0.0 --port 8008 &
 
 # Start the API
-cd api && uvicorn src.serge.main:api_app --reload --host 0.0.0.0 --port 9124 --root-path /api/ &
-
-# Wait for any process to exit
-wait -n
-# Exit with status of process that exited first
-exit $?
+cd /usr/src/app/api || exit 1
+uvicorn src.serge.main:api_app --reload --host 0.0.0.0 --port 9124 --root-path /api/ || {
+	echo 'Failed to start main app'
+	exit 1
+}
