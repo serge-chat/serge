1b84a16999cd6d9fab23922f14d9357b9c1e5409
Juan Calderon-Perez <jgcalderonperez@protonmail.com>
2023-08-06 20:46:27 -0400

    Format config file

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -30,6 +30,6 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
-    "svelte/no-at-html-tags": "off"
-  }
+    "svelte/no-at-html-tags": "off",
+  },
 };
