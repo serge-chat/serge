d4741770a1f6327d3bc6303aa803e276a659833e
Juan Calderon-Perez <jgcalderonperez@protonmail.com>
2023-09-04 01:49:28 -0400

    Change shortcut to trigger question to Enter key

--- web/src/routes/chat/[id]/+page.svelte
+++ web/src/routes/chat/[id]/+page.svelte
@@ -76,7 +76,7 @@
   }
 
   async function handleKeyDown(event: KeyboardEvent) {
-    if (event.key === "Enter" && event.ctrlKey) {
+    if (event.key === "Enter" && !event.shiftKey) {
       await askQuestion();
     }
   }
