02ffab2c1649e1d1477ff041acb95fe5fd6d6083
Juan Calderon-Perez <jgcalderonperez@protonmail.com>
2024-02-13 23:32:35 -0500

    Fixes to UI rendering, fixes to dev.sh

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -200,52 +200,71 @@
     <ul
       class="my-1 w-full flex-grow overflow-y-auto no-scrollbar firefox-no-scrollbar ie-edge-no-scrollbar"
     >
-      {#each data.chats as chat (chat.id)}
-        <li in:fly={{ x: -100, duration: 900 }}>
-          <a
-            href={"/chat/" + chat.id}
-            class="group hover:from-base-100 hover:text-base-content flex items-center rounded-lg py-2 pl-2 text-base font-normal hover:bg-gradient-to-r hover:to-transparent"
-            class:bg-base-300={id === chat.id}
-          >
-            <div class="flex w-full flex-col">
-              <div class="flex w-full flex-col items-start justify-start">
-                <div
-                  class="relative flex w-full flex-row items-center justify-between"
-                >
-                  <div class="flex flex-col">
-                    <p class="text-sm font-light">
-                      {truncate(chat.subtitle, 42)}
-                    </p>
-                    <span class="text-xs font-semibold">{chat.model}</span>
-                    <span class="text-xs"
-                      >{timeSince(chat.created) + " ago"}</span
-                    >
-                  </div>
+      {#if data && data.chats}
+        {#each data.chats as chat (chat.id)}
+          <li in:fly={{ x: -100, duration: 900 }}>
+            <a
+              href={"/chat/" + chat.id}
+              class="group hover:from-base-100 hover:text-base-content flex items-center rounded-lg py-2 pl-2 text-base font-normal hover:bg-gradient-to-r hover:to-transparent"
+              class:bg-base-300={id === chat.id}
+            >
+              <div class="flex w-full flex-col">
+                <div class="flex w-full flex-col items-start justify-start">
                   <div
-                    class="absolute right-0 opacity-0 group-hover:opacity-100 transition"
+                    class="relative flex w-full flex-row items-center justify-between"
                   >
-                    <!-- {#if $page.params.id === chat.id} -->
-                    {#if deleteConfirm}
-                      <div class="flex flex-row items-center">
-                        <button
-                          name="confirm-delete"
-                          class="btn-ghost btn-sm btn"
-                          on:click|preventDefault={() => deleteChat(chat.id)}
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 16 16"
-                            width="16"
-                            height="16"
+                    <div class="flex flex-col">
+                      <p class="text-sm font-light">
+                        {truncate(chat.subtitle, 42)}
+                      </p>
+                      <span class="text-xs font-semibold">{chat.model}</span>
+                      <span class="text-xs"
+                        >{timeSince(chat.created) + " ago"}</span
+                      >
+                    </div>
+                    <div
+                      class="absolute right-0 opacity-0 group-hover:opacity-100 transition"
+                    >
+                      <!-- {#if $page.params.id === chat.id} -->
+                      {#if deleteConfirm}
+                        <div class="flex flex-row items-center">
+                          <button
+                            name="confirm-delete"
+                            class="btn-ghost btn-sm btn"
+                            on:click|preventDefault={() => deleteChat(chat.id)}
                           >
-                            <path
-                              class="fill-base-content"
-                              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm1.5 0a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Zm10.28-1.72-4.5 4.5a.75.75 0 0 1-1.06 0l-2-2a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018l1.47 1.47 3.97-3.97a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042Z"
-                            />
-                          </svg>
-                        </button>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 16 16"
+                              width="16"
+                              height="16"
+                            >
+                              <path
+                                class="fill-base-content"
+                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm1.5 0a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Zm10.28-1.72-4.5 4.5a.75.75 0 0 1-1.06 0l-2-2a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018l1.47 1.47 3.97-3.97a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042Z"
+                              />
+                            </svg>
+                          </button>
+                          <button
+                            name="cancel-delete"
+                            class="btn-ghost btn-sm btn"
+                            on:click|preventDefault={toggleDeleteConfirm}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 16 16"
+                              width="16"
+                              height="16"
+                            >
+                              <path
+                                class="fill-base-content"
+                                d="M2.344 2.343h-.001a8 8 0 0 1 11.314 11.314A8.002 8.002 0 0 1 .234 10.089a8 8 0 0 1 2.11-7.746Zm1.06 10.253a6.5 6.5 0 1 0 9.108-9.275 6.5 6.5 0 0 0-9.108 9.275ZM6.03 4.97 8 6.94l1.97-1.97a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L9.06 8l1.97 1.97a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L8 9.06l-1.97 1.97a.749.749 0 0 1-1.275-.326.749.749 0 0 1 .215-.734L6.94 8 4.97 6.03a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018Z"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                      {:else}
                         <button
-                          name="cancel-delete"
                           class="btn-ghost btn-sm btn"
                           on:click|preventDefault={toggleDeleteConfirm}
                         >
@@ -257,37 +276,20 @@
                           >
                             <path
                               class="fill-base-content"
-                              d="M2.344 2.343h-.001a8 8 0 0 1 11.314 11.314A8.002 8.002 0 0 1 .234 10.089a8 8 0 0 1 2.11-7.746Zm1.06 10.253a6.5 6.5 0 1 0 9.108-9.275 6.5 6.5 0 0 0-9.108 9.275ZM6.03 4.97 8 6.94l1.97-1.97a.749.749 0 0 1 1.275.326.749.749 0 0 1-.215.734L9.06 8l1.97 1.97a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L8 9.06l-1.97 1.97a.749.749 0 0 1-1.275-.326.749.749 0 0 1 .215-.734L6.94 8 4.97 6.03a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018Z"
+                              d="M11 1.75V3h2.25a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1 0-1.5H5V1.75C5 .784 5.784 0 6.75 0h2.5C10.216 0 11 .784 11 1.75ZM4.496 6.675l.66 6.6a.25.25 0 0 0 .249.225h5.19a.25.25 0 0 0 .249-.225l.66-6.6a.75.75 0 0 1 1.492.149l-.66 6.6A1.748 1.748 0 0 1 10.595 15h-5.19a1.75 1.75 0 0 1-1.741-1.575l-.66-6.6a.75.75 0 1 1 1.492-.15ZM6.5 1.75V3h3V1.75a.25.25 0 0 0-.25-.25h-2.5a.25.25 0 0 0-.25.25Z"
                             />
                           </svg>
                         </button>
-                      </div>
-                    {:else}
-                      <button
-                        class="btn-ghost btn-sm btn"
-                        on:click|preventDefault={toggleDeleteConfirm}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 16 16"
-                          width="16"
-                          height="16"
-                        >
-                          <path
-                            class="fill-base-content"
-                            d="M11 1.75V3h2.25a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1 0-1.5H5V1.75C5 .784 5.784 0 6.75 0h2.5C10.216 0 11 .784 11 1.75ZM4.496 6.675l.66 6.6a.25.25 0 0 0 .249.225h5.19a.25.25 0 0 0 .249-.225l.66-6.6a.75.75 0 0 1 1.492.149l-.66 6.6A1.748 1.748 0 0 1 10.595 15h-5.19a1.75 1.75 0 0 1-1.741-1.575l-.66-6.6a.75.75 0 1 1 1.492-.15ZM6.5 1.75V3h3V1.75a.25.25 0 0 0-.25-.25h-2.5a.25.25 0 0 0-.25.25Z"
-                          />
-                        </svg>
-                      </button>
-                    {/if}
-                    <!-- {/if} -->
+                      {/if}
+                      <!-- {/if} -->
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </a>
-        </li>
-      {/each}
+            </a>
+          </li>
+        {/each}
+      {/if}
     </ul>
     <div class="w-full border-t border-base-content/[.2] pt-1">
       {#if deleteAllConfirm}
