d91dd4f68917dc24edccb010101670ad689b5d0d
Juan Calderon-Perez <jgcalderonperez@protonmail.com>
2024-02-13 22:31:05 -0500

    Fix duplicated ports

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -58,8 +58,8 @@ npm run dev -- --host 0.0.0.0 --port 8008 &
 
 # Start the API
 cd /usr/src/app/api || exit 1
-hypercorn_cmd="hypercorn src.serge.main:app --bind 0.0.0.0:8008"
-[ "$SERGE_ENABLE_IPV6" = true ] && hypercorn_cmd+=" --bind [::]:8008"
+hypercorn_cmd="hypercorn src.serge.main:app --bind 0.0.0.0:9124"
+[ "$SERGE_ENABLE_IPV6" = true ] && hypercorn_cmd+=" --bind [::]:9124"
 
 $hypercorn_cmd || {
 	echo 'Failed to start main app'
