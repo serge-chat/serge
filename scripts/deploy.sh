d2a393091ca4b0728bf90d03a7ca669761040c43
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-02-24 03:23:27 +0100

    Update deploy.sh
    Now IPV4 is activate by deflault
    We can activate IPV4+IPV6 or IPV6 only

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -51,7 +51,11 @@ redis_process=$!
 # Start the API
 cd /usr/src/app/api || exit 1
 hypercorn_cmd="hypercorn src.serge.main:app --bind 0.0.0.0:8008"
-[ "$SERGE_ENABLE_IPV6" = true ] && hypercorn_cmd+=" --bind [::]:8008"
+if [ "$SERGE_ENABLE_IPV6" = true ] && [ "$SERGE_ENABLE_IPV4" != true ]; then
+	hypercorn_cmd="hypercorn src.serge.main:app --bind [::]:8008"
+elif [ "$SERGE_ENABLE_IPV4" = true ] && [ "$SERGE_ENABLE_IPV6" = true ]; then
+    	hypercorn_cmd="hypercorn src.serge.main:app --bind 0.0.0.0:8008 --bind [::]:8008"
+fi
 
 $hypercorn_cmd || {
 	echo 'Failed to start main app'
