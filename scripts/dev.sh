e1f966ace3da832945b5757829f5d3dda63aa7db
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-02-24 17:41:51 +0100

    Fix ipv4/ipv6 modes (#1153)
    * Update serge.env
    
    Add ISERGE_ENABLE_IPV4
    
    * Update deploy.sh
    
    Now IPV4 is activate by deflault
    We can activate IPV4+IPV6 or IPV6 only
    
    * Update dev.sh
    
    Now ipv4 is activated by default but we can also activate ipv4+ipv6 or ipv6 only
    
    * Update dev.sh
    
    fix port for ipv4
    
    * Update serge.env
    
    fix SERGE_ENABLE_IPV4 value
    
    * Update deploy.sh
    
    code formating
    
    * Update dev.sh
    
    code formating
    
    * Update dev.sh
    
    bugfix
    
    * Update serge.env
    
    ---------
    
    Co-authored-by: Juan Calderon-Perez <835733+gaby@users.noreply.github.com>

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -58,8 +58,12 @@ npm run dev -- --host 0.0.0.0 --port 8008 &
 
 # Start the API
 cd /usr/src/app/api || exit 1
-hypercorn_cmd="hypercorn src.serge.main:api_app --reload --bind 0.0.0.0:9124"
-[ "$SERGE_ENABLE_IPV6" = true ] && hypercorn_cmd+=" --bind [::]:9124"
+hypercorn_cmd="hypercorn src.serge.main:api_app --bind 0.0.0.0:9124"
+if [ "$SERGE_ENABLE_IPV6" = true ] && [ "$SERGE_ENABLE_IPV4" != true ]; then
+	hypercorn_cmd="hypercorn src.serge.main:api_app --bind [::]:9124"
+elif [ "$SERGE_ENABLE_IPV4" = true ] && [ "$SERGE_ENABLE_IPV6" = true ]; then
+	hypercorn_cmd="hypercorn src.serge.main:api_app --bind 0.0.0.0:9124 --bind [::]:9124"
+fi
 
 $hypercorn_cmd || {
 	echo 'Failed to start main app'
