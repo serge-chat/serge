175abebc042a3119a4a8e3920ecbafbd1dbf9263
Jon Pro <jonpro03@gmail.com>
2024-06-04 15:43:50 -0500

    Get db url from env var

--- api/src/serge/models/settings.py
+++ api/src/serge/models/settings.py
@@ -1,9 +1,15 @@
+from os import getenv
 from pydantic import BaseSettings
 
-
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str = "sqlite:///./sql_app.db"
+    SQLALCHEMY_DATABASE_URL: str = "sqlite:////data/db/sql_app.db"
     NODE_ENV: str = "development"
+
+    def __init__(self):
+        db_url = getenv("SQLALCHEMY_DATABASE_URL", None)
+        if db_url:
+            self.SQLALCHEMY_DATABASE_URL = db_url
+        super(Settings, self).__init__()
 
     class Config:
         orm_mode = True
